115cc31a13441317ebfd0eb01687479de744aeb4
Developer <markevich_maria@bk.ru>
2023-03-07 22:02:33 +0300

    Создаем сериализатор, что это, написано внутри

---
+++ backend/backend/Jewelery/serializers.py
@@ -0,0 +1,16 @@
+# Serializer в Django - это инструмент, который позволяет преобразовывать
+# данные из моделей Django в форматы, которые могут быть переданы через
+# API или сохранены в базе данных. Он также может выполнять обратное преобразование,
+# то есть преобразование данных из формата API обратно в модели Django.
+# Serializer является важным компонентом в построении веб-приложений на Django,
+# особенно при работе с RESTful API.
+
+from rest_framework.serializers import ModelSerializer
+
+from Jewelery.models import Jewelery
+
+
+class JewelerySerializer(ModelSerializer):
+    class Meta:
+        model = Jewelery
+        fields = ('id', 'name', 'price')

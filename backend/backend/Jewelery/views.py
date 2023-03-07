4f12eea10bde97c618cd9f677468c1a695673af1
Developer <markevich_maria@bk.ru>
2023-03-07 22:14:26 +0300

    Это не как представление в БД, это штука Джанго. queryset = Jewelery.objects.all() - хранит таблицей объекты из модели Jewelery serializer_class = JewelerySerializer - превращает данные в json (данные берет из queryset). Сама модель описана в serializers.py

--- backend/backend/Jewelery/views.py
+++ backend/backend/Jewelery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
+from Jewelery.models import Jewelery
+from Jewelery.serializers import JewelerySerializer
+
+
+class JeweleryListView(RetrieveUpdateDestroyAPIView):
+    queryset = Jewelery.objects.all()
+    serializer_class = JewelerySerializer

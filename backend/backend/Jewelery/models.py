4e6bc78c33df3e8c3ac4c5e7c453576de2056c80
Developer <markevich_maria@bk.ru>
2023-03-07 22:28:48 +0300

    Только положительная цена

--- backend/backend/Jewelery/models.py
+++ backend/backend/Jewelery/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Jewelery(models.Model):
     name = models.CharField(max_length=300)
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()

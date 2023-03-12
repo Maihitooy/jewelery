31d1c3c3d9e8cc11dddebdde45c94bc76a6822b1
Developer <markevich_maria@bk.ru>
2023-03-12 18:51:28 +0300

    Сам создал, когда мы создали проект Vue + потестили App.vue и цикл

---
+++ frontend/frontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="item in x" v-bind:key="item" class="d">
+    <h1>THE BEST in the World!</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      x: 10,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.d{
+  color: red;
+  background-color: blue;
+  width: 20%;
+  height: 20%;
+  box-shadow: #5cb85c;
+  margin: auto;
+}
+</style>
